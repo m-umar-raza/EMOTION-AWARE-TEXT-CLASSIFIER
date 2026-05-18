71ae9f825b0e108c91b052aabd07a93b643a14c2
m-umar-raza <154897399+m-umar-raza@users.noreply.github.com>
2026-05-18 17:28:09 +0200

    Refactor VADER score functions for clarity

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,21 @@
-# Utils
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 
 analyzer = SentimentIntensityAnalyzer()
 
+
 def get_vader_score(text):
-    score = analyzer.polarity_scores(text)
-    return score['compound']
+    scores = analyzer.polarity_scores(str(text))
+    return scores["compound"]
+
+
+def get_vader_label(text):
+    score = get_vader_score(text)
+
+    if score >= 0.05:
+        return "positive"
+
+    if score <= -0.05:
+        return "negative"
+
+    return "neutral"
